3eacbb2e571762d964ec8733fecf7db5b7800334
Nayeon Kim <inourbubble2@gmail.com>
2021-10-25 04:13:20 +0900

    Docker 설정을 완료한다

--- app.py
+++ app.py
@@ -16,3 +16,8 @@ def create_app():
     app.register_blueprint(restaurant.app)
 
     return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', debug=True, port=5000)
